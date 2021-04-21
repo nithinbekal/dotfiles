acbc73926ca7063f44ab82a5e09f908040d32f4d
Nithin Bekal <nithin.bekal@shopify.com>
2021-04-21 17:15:55 -0400

    Install plug for neovim

--- install.sh
+++ install.sh
@@ -46,3 +46,7 @@ echo "Setting up Neovim config"
 mkdir -p $HOME/.config/nvim
 rm $HOME/.config/nvim/init.vim
 ln -s $PWD/init.vim $HOME/.config/nvim/init.vim
+
+echo "Installing plug for neovim"
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
