14ab06c869d8e4464d5faddb9c656d9855602c9d
Nithin Bekal <nithin.bekal@shopify.com>
2023-02-08 10:22:09 -0500

    nvim: Switch leader to comma

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -129,8 +129,8 @@ vim.o.completeopt = 'menuone,noselect'
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = ','
+vim.g.maplocalleader = ','
 
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
