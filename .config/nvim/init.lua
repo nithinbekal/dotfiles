5dde92f709ef6820e05342fd17591f587c497696
Nithin Bekal <nithin.bekal@shopify.com>
2023-02-10 13:42:18 -0500

    nvim: create init.lua with setting options

---
+++ .config/nvim/init.lua
@@ -0,0 +1,30 @@
+
+
+-- Setting options
+
+vim.opt.autoindent = true             -- Indent: Copy indent from current line when starting new line
+vim.opt.colorcolumn = '80,120'        -- Show vertical bar to indicate 80/120 chars
+vim.opt.cursorline = true             -- Highlight the cursor line
+vim.opt.expandtab = true              -- Use spaces to insert a tab
+vim.opt.fillchars = 'eob: '           -- Hide ~ in line number column after end of buffer
+vim.opt.grepprg = 'rg --vimgrep'      -- Use ripgrep for file search
+vim.opt.laststatus = 2                -- Always show status line
+vim.opt.list = true                   -- Show tabs and trailing whitespace
+vim.opt.listchars = 'tab:>-,trail:·'  -- Set chars to show for tabs or trailing whitespace
+vim.opt.shiftround = true             -- Indentation: When at 3 spaces, >> takes to 4, not 5
+vim.opt.shiftwidth = 2                -- Tab settings - Use 2 spaces for each indent level
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.updatetime = 200              -- Reduce updatetime
+vim.opt.wildmode = 'list:longest'     -- Completion mode: complete till longest common string
+
+-- Line numbers: Show current line, but use relative numbers elsewhere
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Search
+vim.opt.hlsearch = true               -- Highlight results
+vim.opt.incsearch = true              -- Show results as you type
+vim.opt.ignorecase = true             -- Ignore case
+vim.opt.smartcase = true              -- unless uppercase chars are given
+
