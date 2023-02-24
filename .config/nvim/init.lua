ea4259b8d6abf02bb6d5a990cda513866d418683
Nithin Bekal <nithin.bekal@shopify.com>
2023-02-24 13:57:35 -0500

    nvim: Show hovering docs

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -103,6 +103,17 @@ require("lazy").setup({
 vim.cmd.colorscheme('onedark')
 
 
+function _G.show_docs()
+    local cw = vim.fn.expand('<cword>')
+    if vim.fn.index({'vim', 'help'}, vim.bo.filetype) >= 0 then
+        vim.api.nvim_command('h ' .. cw)
+    elseif vim.api.nvim_eval('coc#rpc#ready()') then
+        vim.fn.CocActionAsync('doHover')
+    else
+        vim.api.nvim_command('!' .. vim.o.keywordprg .. ' ' .. cw)
+    end
+end
+vim.keymap.set("n", "K", '<CMD>lua _G.show_docs()<CR>', {silent = true})
 -- User commands
 
 -- Commonly mistyped commands
