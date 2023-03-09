3f2123dbf8956c00be659f9c28b80cbe8b54ad9e
Nithin Bekal <nithin.bekal@shopify.com>
2023-03-09 12:44:06 -0500

    nvim: show file path in status line

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -82,6 +82,7 @@ require("lazy").setup({
         theme = 'auto',
         component_separators = '|',
         section_separators = '',
+        path = 1, -- show relative file path
       }
     })
     end,
