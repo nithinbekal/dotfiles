1a0e09d875112ca2d46930e2bb30b26a6bc2067a
Nithin Bekal <nithin.bekal@shopify.com>
2023-08-15 15:01:04 -0400

    Extract echo into a function

--- install.sh
+++ install.sh
@@ -2,56 +2,60 @@
 
 set -e
 
+current_status() {
+  printf "\e[32m✔\e[0m %s\n" "$1"
+}
+
 if [ $SPIN ]; then
-  echo "▶︎ Installing packages"
+  current_status "Installing packages"
   sudo apt-get install -y neovim ripgrep
 fi
 
-echo "▶︎ Installing oh-my-zsh"
+current_status "Installing ohmyzsh"
 if [ -d ~/.oh-my-zsh ]; then
-  echo "Found ~/.oh-my-zsh - skipping this step"
+  current_status "Found ~/.oh-my-zsh - skipping this step"
 else
   sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 fi
 
-echo "▶︎ Installing dotfiles"
+current_status "Installing dotfiles"
 
 dotfiles=(.ctags .gemrc .pryrc .railsrc .vimrc .zshrc)
 
 for file in "${dotfiles[@]}"
 do
-  echo "▶︎ Linking ${file}"
+  current_status "Linking ${file}"
   rm -f ~/$file
   ln -s ~/dotfiles/$file ~/$file
 done
 
-echo "▶︎ Linking .vim directory"
+current_status "Linking .vim directory"
 
 mkdir -p ~/.vim
 mkdir -p ~/.vim/tmp
 
 ln -sf ~/dotfiles/.vim/ultisnips ~/.vim/ultisnips
 
-echo "▶︎ Setting up Neovim config"
+current_status "Setting up Neovim config"
 
 mkdir -p ~/.config/nvim
 ln -sf ~/dotfiles/.config/nvim/init.lua ~/.config/nvim/init.lua
 ln -sf ~/dotfiles/.config/nvim/coc-settings.json ~/.config/nvim/coc-settings.json
 
-echo "▶︎ Installing lazy.nvim for neovim"
+current_status "Installing lazy.nvim for neovim"
 
 nvim -c 'q'
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
   if ! which brew > /dev/null; then
-    echo "▶︎ Installing homebrew"
+    current_status "Installing homebrew"
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
   fi;
 
   eval "$(/opt/homebrew/bin/brew shellenv)"
 
-  echo "▶︎ Installing via brew"
+  current_status "Installing via brew"
   brew install neovim fzf ripgrep
 fi
 
-echo "✅ Installation successful"
+current_status "Installation successful 🚀"
