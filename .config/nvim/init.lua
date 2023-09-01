0df2bb4f4ada48790995ab87918f62bec1901d5d
Nithin Bekal <nithin.bekal@shopify.com>
2023-09-01 10:27:23 -0400

    nvim: Move coc keymaps into lazy config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -77,6 +77,45 @@ require("lazy").setup({
   {
     'neoclide/coc.nvim',
     branch = 'release',
+    config = function()
+      vim.keymap.set("n", "[g", "<Plug>(coc-diagnostic-prev)", {silent = true})
+      vim.keymap.set("n", "]g", "<Plug>(coc-diagnostic-next)", {silent = true})
+      vim.keymap.set("n", "gd", "<Plug>(coc-definition)", {silent = true})
+      vim.keymap.set("n", "gr", "<Plug>(coc-references)", {silent = true})
+
+      function _G.check_back_space()
+        local col = vim.fn.col('.') - 1
+        return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
+      end
+
+      local opts = {noremap = true, expr = true, replace_keycodes = false}
+
+      vim.keymap.set("i", "<TAB>",
+        [[coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "<TAB>" : coc#refresh()]], opts)
+
+      vim.keymap.set("i", "<S-TAB>", [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]], opts)
+
+      -- Make <CR> to accept selected completion item or notify coc.nvim to format
+      -- <C-g>u breaks current undo, please make your own choice
+      vim.keymap.set("i", "<cr>",
+        [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]], opts)
+
+      function _G.show_docs()
+        local cw = vim.fn.expand('<cword>')
+        if vim.fn.index({'vim', 'help'}, vim.bo.filetype) >= 0 then
+          vim.api.nvim_command('h ' .. cw)
+        elseif vim.api.nvim_eval('coc#rpc#ready()') then
+          vim.fn.CocActionAsync('doHover')
+        else
+          vim.api.nvim_command('!' .. vim.o.keywordprg .. ' ' .. cw)
+        end
+      end
+
+      vim.keymap.set("n", "K", '<CMD>lua _G.show_docs()<CR>', {silent = true})
+
+      vim.keymap.set('n', '<leader>mv', ':CocCommand workspace.renameCurrentFile<cr>',
+        { desc = 'Rename current file' })
+    end,
   },
 
   {
@@ -112,46 +151,6 @@ require("lazy").setup({
 
   'wsdjeg/vim-fetch',
 })
-
--- coc.nvim config
-
-vim.keymap.set("n", "[g", "<Plug>(coc-diagnostic-prev)", {silent = true})
-vim.keymap.set("n", "]g", "<Plug>(coc-diagnostic-next)", {silent = true})
-
-vim.keymap.set("n", "gd", "<Plug>(coc-definition)", {silent = true})
-vim.keymap.set("n", "gr", "<Plug>(coc-references)", {silent = true})
-
-function _G.check_back_space()
-    local col = vim.fn.col('.') - 1
-    return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
-end
-
-local opts = {noremap = true, expr = true, replace_keycodes = false}
-
-vim.keymap.set("i", "<TAB>",
-  [[coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "<TAB>" : coc#refresh()]], opts)
-
-vim.keymap.set("i", "<S-TAB>", [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]], opts)
-
--- Make <CR> to accept selected completion item or notify coc.nvim to format
--- <C-g>u breaks current undo, please make your own choice
-vim.keymap.set("i", "<cr>",
-  [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]], opts)
-
-
-function _G.show_docs()
-    local cw = vim.fn.expand('<cword>')
-    if vim.fn.index({'vim', 'help'}, vim.bo.filetype) >= 0 then
-        vim.api.nvim_command('h ' .. cw)
-    elseif vim.api.nvim_eval('coc#rpc#ready()') then
-        vim.fn.CocActionAsync('doHover')
-    else
-        vim.api.nvim_command('!' .. vim.o.keywordprg .. ' ' .. cw)
-    end
-end
-
-vim.keymap.set("n", "K", '<CMD>lua _G.show_docs()<CR>', {silent = true})
-
 
 -- User commands
 
@@ -226,8 +225,6 @@ vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = tr
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 vim.keymap.set('n', '<leader>dd', '<Plug>(devdocs-under-cursor)', { desc = 'Open devdocs.io' })
-vim.keymap.set('n', '<leader>mv', ':CocCommand workspace.renameCurrentFile<cr>',
-  { desc = 'Rename current file' })
 vim.keymap.set('n', '<leader>nh', ':nohl<cr>', { desc = 'No highlight' })
 vim.keymap.set('n', '<leader>o', ':only<cr>', { desc = 'Only keep current pane' })
 vim.keymap.set('n', '<leader>pp', '"+p', { desc = 'Paste from clipboard' })
