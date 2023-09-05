c52edccae79b02c24029df067b710ecd5ec373cc
Nithin Bekal <nithin.bekal@shopify.com>
2023-09-05 09:43:47 -0400

    remove coc.nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -111,50 +111,6 @@ local plugins = {
   },
 
   {
-    'neoclide/coc.nvim',
-    branch = 'release',
-    config = function()
-      vim.keymap.set("n", "[g", "<Plug>(coc-diagnostic-prev)", {silent = true})
-      vim.keymap.set("n", "]g", "<Plug>(coc-diagnostic-next)", {silent = true})
-      vim.keymap.set("n", "gd", "<Plug>(coc-definition)", {silent = true})
-      vim.keymap.set("n", "gr", "<Plug>(coc-references)", {silent = true})
-
-      function _G.check_back_space()
-        local col = vim.fn.col('.') - 1
-        return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
-      end
-
-      local opts = {noremap = true, expr = true, replace_keycodes = false}
-
-      vim.keymap.set("i", "<TAB>",
-        [[coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "<TAB>" : coc#refresh()]], opts)
-
-      vim.keymap.set("i", "<S-TAB>", [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]], opts)
-
-      -- Make <CR> to accept selected completion item or notify coc.nvim to format
-      -- <C-g>u breaks current undo, please make your own choice
-      vim.keymap.set("i", "<cr>",
-        [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]], opts)
-
-      function _G.show_docs()
-        local cw = vim.fn.expand('<cword>')
-        if vim.fn.index({'vim', 'help'}, vim.bo.filetype) >= 0 then
-          vim.api.nvim_command('h ' .. cw)
-        elseif vim.api.nvim_eval('coc#rpc#ready()') then
-          vim.fn.CocActionAsync('doHover')
-        else
-          vim.api.nvim_command('!' .. vim.o.keywordprg .. ' ' .. cw)
-        end
-      end
-
-      vim.keymap.set("n", "K", '<CMD>lua _G.show_docs()<CR>', {silent = true})
-
-      vim.keymap.set('n', '<leader>mv', ':CocCommand workspace.renameCurrentFile<cr>',
-        { desc = 'Rename current file' })
-    end,
-  },
-
-  {
     'nvim-lualine/lualine.nvim',
     config = function() require('lualine').setup({
       options = {
