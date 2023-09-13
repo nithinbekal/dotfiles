06b7c37cf40627a84ff37a8634701f4cc66c378c
Nithin Bekal <nithin.bekal@shopify.com>
2023-09-13 09:37:18 -0400

    nvim: fix GBrowse mapping

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -174,7 +174,7 @@ local plugins = {
       { "<leader>gbl", ":Git blame<cr>", desc = "Git blame" },
       { "<leader>ghp", ":!/opt/dev/bin/dev open pr &<cr><cr>", desc = "Github PR" },
       { "<leader>gs", ":Git<cr>", desc = "Git status" },
-      { "<leader>gbr", ":Gbrowse<cr>", desc = "Git browse", mode = { "n", "v" } },
+      { "<leader>gbr", ":GBrowse<cr>", desc = "Git browse", mode = { "n", "v" } },
     },
   },
 
