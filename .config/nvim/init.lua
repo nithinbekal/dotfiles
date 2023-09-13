c385730413cf2cdbdbf1ab7b6b354e00569c5d5b
Nithin Bekal <nithin.bekal@shopify.com>
2023-09-13 13:52:41 -0400

    nvim: remove unused treesitter keymaps

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -322,9 +322,6 @@ require("nvim-treesitter.configs").setup {
       enable = true,
       lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
       keymaps = {
-        -- You can use the capture groups defined in textobjects.scm
-        ["aa"] = "@parameter.outer",
-        ["ia"] = "@parameter.inner",
         ["af"] = "@function.outer",
         ["if"] = "@function.inner",
         ["ac"] = "@class.outer",
@@ -349,15 +346,6 @@ require("nvim-treesitter.configs").setup {
       goto_previous_end = {
         ["[M"] = "@function.outer",
         ["[]"] = "@class.outer",
-      },
-    },
-    swap = {
-      enable = true,
-      swap_next = {
-        ["<leader>a"] = "@parameter.inner",
-      },
-      swap_previous = {
-        ["<leader>A"] = "@parameter.inner",
       },
     },
   },
