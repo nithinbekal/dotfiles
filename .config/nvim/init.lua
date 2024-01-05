15cc0aa462d99e7991a5eadf322bd41e1dfee9a9
Nithin Bekal <nithin.bekal@shopify.com>
2024-01-05 11:56:44 -0500

    nvim: set up nvim-treesitter-config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -158,6 +158,7 @@ local plugins = {
   {
     "nvim-treesitter/nvim-treesitter",
     dependencies = {
+      "nvim-treesitter/nvim-treesitter-context",
       "nvim-treesitter/nvim-treesitter-textobjects",
       "omnisyle/nvim-hidesig",
     },
