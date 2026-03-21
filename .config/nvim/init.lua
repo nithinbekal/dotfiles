daeedceb216431f7c9e11cc1c16df9ab1ad82971
Nithin Bekal <nithinbekal@gmail.com>
2026-03-21 00:46:13 -0400

    nvim: simplify cmp setup

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -65,6 +65,61 @@ local plugins = {
       "hrsh7th/cmp-nvim-lsp",
       "rafamadriz/friendly-snippets",
     },
+    config = function()
+      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+
+      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+
+      require("luasnip.loaders.from_vscode").lazy_load()
+      luasnip.config.setup {}
+
+      cmp.setup {
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert {
+          ["<C-n>"] = cmp.mapping.select_next_item(),
+          ["<C-p>"] = cmp.mapping.select_prev_item(),
+          ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete {},
+          ["<CR>"] = cmp.mapping.confirm {
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+          },
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_locally_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.locally_jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+        },
+        sources = {
+          { name = "copilot" },
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+        },
+        enabled = function()
+          return vim.bo.filetype ~= "markdown"
+        end,
+      }
+    end,
   },
 
   {
@@ -306,62 +361,6 @@ require("lazy").setup({
 })
 
 
-
--- Autocomplete setup
-
-local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-local cmp = require("cmp")
-local luasnip = require("luasnip")
-
-cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-
-require("luasnip.loaders.from_vscode").lazy_load()
-luasnip.config.setup {}
-
-cmp.setup {
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert {
-    ["<C-n>"] = cmp.mapping.select_next_item(),
-    ["<C-p>"] = cmp.mapping.select_prev_item(),
-    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-Space>"] = cmp.mapping.complete {},
-    ["<CR>"] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
-    },
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_locally_jumpable() then
-        luasnip.expand_or_jump()
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.locally_jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-  },
-  sources = {
-    { name = "copilot" },
-    { name = "nvim_lsp" },
-    { name = "luasnip" },
-  },
-  enabled = function ()
-    return vim.bo.filetype ~= "markdown"
-  end,
-}
 
 vim.diagnostic.config({
   underline = { severity = { max = vim.diagnostic.severity.INFO } },
