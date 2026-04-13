00db267ec47eed19d1ad37c4c5c959c584b288ce
Nithin Bekal <nithinbekal@gmail.com>
2026-04-13 16:10:22 -0400

    nvim: fix treesitter config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -242,10 +242,15 @@ local plugins = {
     "nvim-treesitter/nvim-treesitter",
     branch = "main",
     build = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter").setup({
-        ensure_installed = { "lua", "ruby", "vimdoc" },
-      })
+    init = function()
+      local ensureInstalled = { "lua", "ruby", "rust", "vimdoc" }
+      local alreadyInstalled = require("nvim-treesitter.config").get_installed()
+      local parsersToInstall = vim.iter(ensureInstalled)
+        :filter(function(parser)
+          return not vim.tbl_contains(alreadyInstalled, parser)
+        end)
+        :totable()
+      require("nvim-treesitter").install(parsersToInstall)
     end,
   },
 
